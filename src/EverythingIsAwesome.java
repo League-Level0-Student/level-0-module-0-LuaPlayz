caf684e0bca16d305c05d26e3ce1c305b9dca766
League Student <league@iMac-24.attlocal.net>
2019-11-20 17:32:19 -0800

    finished

---
+++ src/EverythingIsAwesome.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesome {
+public static void main(String[] args) {
+String input=JOptionPane.showInputDialog(null, "WUT DO U LIKE?!?!?!?!?!?!");
+JOptionPane.showMessageDialog(null, "THAT'S SOOOOOO AWESOME!!!!!!!!!!!!!!!");
+}
+}
---
+++ src/LastSummer.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class LastSummer {
+public static void main(String[] args) {
+	String input=JOptionPane.showInputDialog(null, "WUT IS UR NAMEE???~ ");
+	JOptionPane.showMessageDialog(null, "I know exactly what you did last summer,"+input+".MUAHAHAHAHAHAHHAHHAHAHAHAHAH!!!");
+}
+}
